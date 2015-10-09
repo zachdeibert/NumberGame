e5f853f885abfcded718f08d0307e597f3cd42ae
Zach Deibert <zachdeibert@gmail.com>
2015-10-09 18:01:31 -0500

    Added cancel button

--- src/js/joiningSplash.js
+++ src/js/joiningSplash.js
@@ -8,12 +8,18 @@ function openJoiningSplash(replace) {
     menu.classList.add("menu");
     menu.classList.add("join-splash");
     var div = document.createElement("div");
+    div.className = "text-box";
     var h1 = document.createElement("h1");
     h1.appendChild(document.createTextNode("Joining Game..."));
     var h2 = document.createElement("h2");
     h2.appendChild(document.createTextNode("Waiting for other players to join"));
+    var cancel = document.createElement("div");
+    cancel.classList.add("btn");
+    cancel.classList.add("btn-danger");
+    cancel.appendChild(document.createTextNode("Cancel"));
     div.appendChild(h1);
     div.appendChild(h2);
+    div.appendChild(cancel);
     menu.appendChild(div);
     if ( replace ) {
         while ( document.body.children.length > 0 ) {
